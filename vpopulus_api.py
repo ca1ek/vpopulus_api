097e69c273990e5fb53949d68ac6f8fd9b8447eb
ca1ek <ca1ek@users.noreply.github.com>
2015-07-01 22:43:08 +0200

    Added in class Citizen with all pulled citizen data

--- vpopulus_api.py
+++ vpopulus_api.py
@@ -1,3 +1,22 @@
 __author__ = 'ca1ek'
 
+import requests
+
+
+class Citizen:
+    def __init__(self, citizen_name):
+        json = requests.get("http://api.vpopulus.net/v1/feeds/citizen.json?name=" + citizen_name).json()
+        self.raw_json = json
+        self.id = json["id"]
+        self.avatar_url = json["avatar-link"]
+        self.wellness = json["wellness"]
+        self.skills = json["skills"]
+        self.citizenship = json["citizenship"]["country"]
+        self.location = json["location"]["country"]
+        self.region = json["location"]["region"]
+        self.company = json["company"]
+        self.party = json["party"]
+        self.army = json["army"]
+        self.newspaper = json["newspaper"]
+        self.date_of_birth = json["date-of-birth"]
 
